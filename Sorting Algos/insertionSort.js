82261ba89c9b03a0d706497504f17aaa74fa5353
sahil immortal <sahilsawal959@gmail.com>
2023-01-24 21:18:19 +0530

    add selection sort

--- Sorting Algos/insertionSort.js
+++ Sorting Algos/insertionSort.js
@@ -1,17 +1,24 @@
-//insertion sort
-//big On2
-var list = [2, 5, 10, 3, 8, 6, 9, 1];
-function insertionSort(arr){
-    console.log(arr);
-    for(let i=1; i<arr.length; i++){
-        let cv=arr[i];
-        for(let j=i-1;j>=0&&arr[j]>cv;j--){
-            arr[j+1]=arr[j];
-
+var list = [5,3,4,1,2];
+function insertionSort(arr)
+{  
+    for(let i=0; i<arr.length; i++){
+        let lowest=i;
+ 
+        for(let j=i+1;j<arr.length; j++){
+        
+            if(arr[lowest]>arr[j]){
+           lowest=j;
+            }  
         }
-        arr[j+1]=cv;
+        if(i!==lowest){
+        // swap(arr,i, lowest);
+            var temp =arr[i];
+            arr[i]=arr[lowest];
+            arr[lowest]=temp;
+        }
     }
     
     return arr;
 }
-console.log(insertionSort(list));
+let val=insertionSort(list);
+console.log(val);
